96ad7de21d2d26634ed9e680ace10f591962d083
michaeldouglassoares <michaeldouglas.10.94@gmail.com>
2023-09-10 22:32:12 -0300

    feat: add file config hosts

--- services/configHost.ts
+++ services/configHost.ts
@@ -1,5 +1,6 @@
 export const configHost = {
-  host: window.location.host.includes("localhost")
-    ? "http://localhost:3015/"
-    : "http://137.184.24.163:3015/",
+  host:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3015/"
+      : "http://137.184.24.163:3015/",
 };
