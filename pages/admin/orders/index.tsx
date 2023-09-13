69e2ae2d9ad9138cbb9d2a299a65a667f9d1b274
michaeldouglassoares <michaeldouglas.10.94@gmail.com>
2023-09-13 02:20:16 -0300

    buttons send email

--- pages/admin/orders/index.tsx
+++ pages/admin/orders/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSidePropsContext, NextPage } from "next";
-import React from "react";
+import React, { useState } from "react";
 
 import nookies from 'nookies';
 
@@ -23,6 +23,8 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import { IOrders, IProduct } from "../../../types/OrderType";
 import { Box, Collapse, IconButton, Typography } from "@mui/material";
+import Swal from "sweetalert2";
+import Loader from "../../../components/Loader";
 
 interface IAdminOrders {
   orders: IOrders[];
@@ -31,6 +33,32 @@ interface IAdminOrders {
 const AdminEvents: NextPage<IAdminOrders> = ({ orders }) => {
 
   const [open, setOpen] = React.useState(false);
+  const [loader, setLoader] = useState<boolean>(false);
+
+  const handleSendEmails = async () => {
+    setLoader(true);
+
+    try {
+
+      const response = await ApiService.post('/calculate', {});
+
+      setLoader(false);
+
+      if (response.status === 200) {
+        Swal.fire({
+          text: `Disparo realizado com sucesso.`,
+          showCancelButton: false,
+          confirmButtonText: 'Fechar',
+          icon: 'success',
+          showCloseButton: true,
+          cancelButtonText: 'Fechar'
+        })
+      }
+
+    } catch (error) {
+      setLoader(false);
+    }
+  }
 
   return (
     <Container>
@@ -38,61 +66,73 @@ const AdminEvents: NextPage<IAdminOrders> = ({ orders }) => {
       <Navigation />
       <ContainerOrderDashboard>
         <ContentTable>
-          {orders?.length > 0 ? (
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell align="center">Hotel</TableCell>
-                    <TableCell align="center">Nome cliente</TableCell>
-                    <TableCell align="center">Email</TableCell>
-                    <TableCell align="center">Celular</TableCell>
-                    <TableCell align="center">Estado</TableCell>
-                    <TableCell align="center">CNPJ</TableCell>
-                    <TableCell align="center">ICMS</TableCell>
-                    <TableCell align="center">Criado em</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {orders.map((order: IOrders) => (
-                    <TableRow key={order.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                      <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box sx={{ margin: 1 }}>
-                          <Typography variant="h6" gutterBottom component="div">
-                            Produtos
-                          </Typography>
-                          <Table size="small" aria-label="purchases">
-                            <TableHead>
-                              <TableRow>
-                                <TableCell>Código produto</TableCell>
-                                <TableCell align="center">IPI</TableCell>
-                                <TableCell align="center">Preço NET</TableCell>
-                                <TableCell align="center">Quantidade</TableCell>
-                                <TableCell align="center">Título</TableCell>
-                                <TableCell align="center">Tipo produto</TableCell>
-                                <TableCell align="center">Criado em</TableCell>
-                              </TableRow>
-                            </TableHead>
-                            <TableBody>
-                              {order?.orders?.map((order: IProduct) => (
-                                <TableRow key={order.id}>
-                                  <TableCell component="th" scope="row">
-                                    {order.product_code}
-                                  </TableCell>
-                                  <TableCell align="center">{order.ipi}</TableCell>
-                                  <TableCell align="center">{order.price_net}</TableCell>
-                                  <TableCell align="center">{order.quantity}</TableCell>
-                                  <TableCell align="center">{order.title}</TableCell>
-                                  <TableCell align="center">{order.type_product}</TableCell>
-                                  <TableCell align="center">{order.type_product}</TableCell>
+          <Box display="flex" justifyContent="flex-end" mb={12}>
+            <IconButton
+              color="primary"
+              aria-label="emails"
+              onClick={handleSendEmails}
+            >
+              Disparar emails
+            </IconButton>
+          </Box>
+          {loader ? (
+            <Loader />
+          ) : (
+            orders?.length > 0 ? (
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ID</TableCell>
+                      <TableCell align="center">Hotel</TableCell>
+                      <TableCell align="center">Nome cliente</TableCell>
+                      <TableCell align="center">Email</TableCell>
+                      <TableCell align="center">Celular</TableCell>
+                      <TableCell align="center">Estado</TableCell>
+                      <TableCell align="center">CNPJ</TableCell>
+                      <TableCell align="center">ICMS</TableCell>
+                      <TableCell align="center">Criado em</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {orders.map((order: IOrders) => (
+                      <TableRow key={order.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                          <Box sx={{ margin: 1 }}>
+                            <Typography variant="h6" gutterBottom component="div">
+                              Produtos
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                              <TableHead>
+                                <TableRow>
+                                  <TableCell>Código produto</TableCell>
+                                  <TableCell align="center">IPI</TableCell>
+                                  <TableCell align="center">Preço NET</TableCell>
+                                  <TableCell align="center">Quantidade</TableCell>
+                                  <TableCell align="center">Título</TableCell>
+                                  <TableCell align="center">Tipo produto</TableCell>
+                                  <TableCell align="center">Criado em</TableCell>
                                 </TableRow>
-                              ))}
-                            </TableBody>
-                          </Table>
-                        </Box>
-                      </Collapse>
-                      {/* <TableCell>
+                              </TableHead>
+                              <TableBody>
+                                {order?.orders?.map((order: IProduct) => (
+                                  <TableRow key={order.id}>
+                                    <TableCell component="th" scope="row">
+                                      {order.product_code}
+                                    </TableCell>
+                                    <TableCell align="center">{order.ipi}</TableCell>
+                                    <TableCell align="center">{order.price_net}</TableCell>
+                                    <TableCell align="center">{order.quantity}</TableCell>
+                                    <TableCell align="center">{order.title}</TableCell>
+                                    <TableCell align="center">{order.type_product}</TableCell>
+                                    <TableCell align="center">{order.type_product}</TableCell>
+                                  </TableRow>
+                                ))}
+                              </TableBody>
+                            </Table>
+                          </Box>
+                        </Collapse>
+                        {/* <TableCell>
                         <IconButton
                           aria-label="expand row"
                           size="small"
@@ -101,29 +141,30 @@ const AdminEvents: NextPage<IAdminOrders> = ({ orders }) => {
                           {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                         </IconButton>
                       </TableCell> */}
-                      <TableCell component="th" scope="row">{order.id}</TableCell>
-                      <TableCell align="center">{order.hotel}</TableCell>
-                      <TableCell align="center">{order.name}</TableCell>
-                      <TableCell align="center">{order.email}</TableCell>
-                      <TableCell align="center">{order.phone}</TableCell>
-                      <TableCell align="center">{order.state}</TableCell>
-                      <TableCell align="center">{order.cnpj}</TableCell>
-                      <TableCell align="center">{order.icms ? 'SIM' : 'NAO'}</TableCell>
-                      <TableCell align="center">{new Intl.DateTimeFormat("pt-BR", {
-                        day: "2-digit",
-                        month: "2-digit",
-                        year: "numeric",
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        second: "2-digit",
-                      }).format(new Date(order.created_at))}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          ) : (
-            <h1>Nenhum pedido localizado</h1>
+                        <TableCell component="th" scope="row">{order.id}</TableCell>
+                        <TableCell align="center">{order.hotel}</TableCell>
+                        <TableCell align="center">{order.name}</TableCell>
+                        <TableCell align="center">{order.email}</TableCell>
+                        <TableCell align="center">{order.phone}</TableCell>
+                        <TableCell align="center">{order.state}</TableCell>
+                        <TableCell align="center">{order.cnpj}</TableCell>
+                        <TableCell align="center">{order.icms ? 'SIM' : 'NAO'}</TableCell>
+                        <TableCell align="center">{new Intl.DateTimeFormat("pt-BR", {
+                          day: "2-digit",
+                          month: "2-digit",
+                          year: "numeric",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          second: "2-digit",
+                        }).format(new Date(order.created_at))}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            ) : (
+              <h1>Nenhum pedido localizado</h1>
+            )
           )}
         </ContentTable>
       </ContainerOrderDashboard>
@@ -151,7 +192,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const response = await ApiService.get('/orders');
 
   const orders = response.data;
-  
+
   return {
     props: { orders },
   };
